7bfd6c10bd8d09734373a9197cda5a803bcdc2e9
Fatih Turhan <m.fatihturhan@gmail.com>
2022-04-12 01:56:05 +0300

    chart btns added

--- src/cart/setCart.js
+++ src/cart/setCart.js
@@ -7,6 +7,7 @@ import setCartDom from "./setCartDom.js";
 // elements
 const amount = document.querySelector(".cart-amount-icon");
 const priceAmount = document.querySelector(".finish-amounth");
+const cartContainer = document.querySelector(".cart-center");
 
 let cart = getLocalStorage("cart");
 
@@ -51,6 +52,13 @@ function addCartsToDom() {
   });
 }
 
+function clearItem(id) {
+  cart = cart.filter((chartItem) => {
+    chartItem.id !== id;
+  });
+  console.log(id);
+}
+
 function updateAmount(id) {
   let newAmount;
   cart = cart.map((cartItem) => {
@@ -62,6 +70,49 @@ function updateAmount(id) {
   });
   return newAmount;
 }
+function decreaseAmount(id) {
+  let newAmount;
+  cart = cart.map((cartItem) => {
+    if (cartItem.id === id) {
+      newAmount = cartItem.amount - 1;
+      cartItem = { ...cartItem, amount: newAmount };
+    }
+    return cartItem;
+  });
+  return newAmount;
+}
+
+function setUpCartFunct() {
+  cartContainer.addEventListener("click", (e) => {
+    const target = e.target;
+    const targetId = e.target.dataset.id;
+    const targetParent = e.target.parentElement;
+    const targetParentId = e.target.parentElement.dataset.id;
+    // remove
+    if (target.classList.contains("clear-btn")) {
+      clearItem(targetId);
+      target.parentElement.parentElement.remove();
+    }
+    // increase
+    if (targetParent.classList.contains("inc-btn")) {
+      const newAmount = updateAmount(targetParentId);
+      targetParent.nextElementSibling.textContent = newAmount;
+    }
+    // decrease
+    if (targetParent.classList.contains("dec-btn")) {
+      const newAmount = decreaseAmount(targetParentId);
+      if (newAmount === 0) {
+        clearItem(targetParentId);
+        targetParent.parentElement.parentElement.remove();
+      } else {
+        targetParent.previousElementSibling.textContent = newAmount;
+      }
+    }
+    showPriceTotal();
+    showProductItemsCount();
+    setLocalStorage("cart", cart);
+  });
+}
 
 const init = () => {
   // update total
@@ -69,6 +120,8 @@ const init = () => {
   showPriceTotal();
   // update cart dom
   addCartsToDom();
+  // inc/dec/remove
+  setUpCartFunct();
 };
 
 window.addEventListener("DOMContentLoaded", () => {
